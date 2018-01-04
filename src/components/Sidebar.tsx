dfc39264b74cd6f3ca4575ae309a9d21abbf4ac4
John Wika Haakseth <johnwika@gmail.com>
2018-01-04 11:59:09 +0100

    Started making my own sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 /* tslint:disable no-any */
 import * as React from 'react';
+import MediaQuery from 'react-responsive';
+
 import { CircularProgress } from 'material-ui/Progress';
-import Drawer from 'material-ui/Drawer';
 import List, { ListItem } from 'material-ui/List';
 import LeftIcon from 'material-ui-icons/ChevronLeft';
 import { LngLat } from 'mapbox-gl';
@@ -38,7 +39,7 @@ class Sidebar extends React.Component<SidebarProps, SidebarState> {
     }
     return <ListItem />;
   }
-  render() {
+  renderList() {
     let closeButtonStyle = styles.closeButton;
     if (this.state.closeButtonHover) {
       closeButtonStyle = styles.closeButtonHover;
@@ -48,60 +49,105 @@ class Sidebar extends React.Component<SidebarProps, SidebarState> {
       resetButtonStyle = styles.closeButtonHover;
     }
     return (
-      <Drawer open={this.props.toggled} type="persistent">
-        <List style={styles.component}>
-          <ListItem
-            button={true}
-            onClick={this.props.close}
-            style={closeButtonStyle}
-            onMouseEnter={() => {
-              this.toggleClosebuttonHover();
-            }}
-            onMouseLeave={() => {
-              this.toggleClosebuttonHover();
-            }}
-          >
-            <LeftIcon />
-          </ListItem>
-          <DestinationCard
-            title={'Origin'}
-            destination={this.props.originPoint}
-          />
-          <DestinationCard
-            title={'Destination'}
-            destination={this.props.destinationPoint}
-          />
-          <DirectionsCard
-            error={this.props.directionsError}
-            steps={this.props.directionSteps}
-          />
-          {this.renderLoader()}
-          <ListItem
-            button={true}
-            onClick={this.props.resetAndClose}
-            style={resetButtonStyle}
-            onMouseEnter={() => {
-              this.toggleResetButtonHover();
-            }}
-            onMouseLeave={() => {
-              this.toggleResetButtonHover();
-            }}
-          >
-            Reset
-          </ListItem>
-        </List>
-      </Drawer>
+      <div>
+        <ListItem
+          button={true}
+          onClick={this.props.close}
+          style={closeButtonStyle}
+          onMouseEnter={() => {
+            this.toggleClosebuttonHover();
+          }}
+          onMouseLeave={() => {
+            this.toggleClosebuttonHover();
+          }}
+        >
+          <LeftIcon />
+        </ListItem>
+        <DestinationCard
+          title={'Origin'}
+          destination={this.props.originPoint}
+        />
+        <DestinationCard
+          title={'Destination'}
+          destination={this.props.destinationPoint}
+        />
+        <DirectionsCard
+          error={this.props.directionsError}
+          steps={this.props.directionSteps}
+        />
+        {this.renderLoader()}
+        <ListItem
+          button={true}
+          onClick={this.props.resetAndClose}
+          style={resetButtonStyle}
+          onMouseEnter={() => {
+            this.toggleResetButtonHover();
+          }}
+          onMouseLeave={() => {
+            this.toggleResetButtonHover();
+          }}
+        >
+          Reset
+        </ListItem>
+      </div>
     );
+  }
+  renderSidebar(smallScreen: boolean) {
+    if (smallScreen) {
+      return (
+        <div
+          style={{
+            ...styles.component,
+            ...styles.componentSmallScreen
+          }}
+        >
+          <List>{this.renderList()}</List>
+        </div>
+      );
+    }
+    return (
+      <div style={styles.component}>
+        <List>{this.renderList()}</List>
+      </div>
+    );
+  }
+  render() {
+    if (this.props.toggled) {
+      return (
+        <div style={{ flex: 1 }}>
+          <MediaQuery query="(min-width: 600px)">
+            {this.renderSidebar(false)}
+          </MediaQuery>
+          <MediaQuery query="(max-width: 600px)">
+            {this.renderSidebar(true)}
+          </MediaQuery>
+        </div>
+      );
+    }
+    return null;
   }
 }
 
 const styles = {
   component: {
+    height: '100%',
+    top: 0,
     width: 250,
-    backgroundColor: 'rgb(240, 240, 240)'
+    position: 'fixed' as 'fixed',
+    zIndex: 2,
+    left: 0,
+    backgroundColor: 'rgb(240, 240, 240)',
+    overflowX: 'hidden' as 'hidden',
+    transition: '0.5s'
+  },
+  componentSmallScreen: {
+    height: '20%' /* 100% Full-height */,
+    width: '100%' /* 0 width - change this with JavaScript */,
+    top: undefined,
+    bottom: 0
   },
   closeButton: {
-    backgroundColor: 'rgb(240, 240, 240)'
+    // backgroundColor: 'rgb(240, 240, 240)'
   },
   closeButtonHover: {
     backgroundColor: 'rgb(220, 220, 220)'
